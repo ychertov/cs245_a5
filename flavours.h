e7b7836ccd31015957ccb7e321614eef045ce488
ychertov <yevgeny.chertov@gmail.com>
2012-07-12 19:32:05 -0400

    vending machine skeleton made

--- flavours.h
+++ flavours.h
@@ -1,6 +1,9 @@
+#ifndef FLAVOURS_H
+#define FLAVOURS_H
 enum Flavours { 
 	BLUES_BLACK_CHERRY, 
 	CLASSICAL_CREAM_SODA, 
 	ROCK_ROOT_BEER,
 	JAZZ_LIME
 };
+#endif
--- vendingmachine.cc
+++ vendingmachine.cc
@@ -1,53 +1,44 @@
-#ifndef __VENDINGMACHINE_H__
-#define __VENDINGMACHINE_H__
-
+#include "vendingmachine.h"
 #include "flavours.h"
 #include "watcard.h"
 #include "printer.h"
 #include "nameserver.h"
 
-class VendingMachine {
-	
-	public:
-		enum Status { BUY, STOCK, FUNDS };
-			/*
-				Pruchase Status:
-					BUY: successful buy
-					STOCK: out of stock
-					FUNDS: insufficient funds			
-			*/
-		VendingMachine( Printer &prt, NameServer &nameServer, unsigned int id, unsigned int sodaCost, unsigned int maxStockParFlavour );
-			/*
-				Set stock to 0
-				Register with nameserver
-			*/
+VendingMachine::VendingMachine( Printer &prt, NameServer &nameServer, unsigned int id, unsigned int sodaCost, unsigned int maxStockParFlavour ) {
+/*
+	Set stock to 0
+	egister with nameserver
+*/
+}
+VendingMachine::~VendingMachine() {
+}
 
-		virtual ~VendingMachine();
+Status VendingMachine::buy( Flavours flavour, WATCard *&card ) {
+/*
+	If the specified soda is unavailable or the student has insufficient funds to purchase the soda, buy returns STOCK
+	or FUNDS; otherwise, WATCard is debited by the cost of a soda and buy returns BUY.
+*/
+}
+unsigned int *VendingMachine::inventory() {
+/*
+	return a pointer to an array containing the amount of each kind of soda currently in the vending machine
+*/
+}
 
-		virtual Status buy( Flavours flavour, WATCard *&card );
-			/*
-				If the specified soda is unavailable or the student has insufficient funds to purchase the soda, buy returns STOCK
-				or FUNDS; otherwise, WATCard is debited by the cost of a soda and buy returns BUY.
-			*/
+void VendingMachine::restocked() {
+/*
+	called to indicate the restocking is complete.
+*/
+}
 
-		virtual unsigned int *inventory();
-			/*
-				return a pointer to an array containing the amount of each kind of soda currently in the vending machine
-			*/
+unsigned int VendingMachine::cost() {
+/*
+	returns the cost of purchasing a soda
+*/
+}
 
-		virtual void restocked();
-			/*
-				called to indicate the restocking is complete.
-			*/
-
-		virtual unsigned int cost();
-			/*
-				returns the cost of purchasing a soda
-			*/
-
-		virtual unsigned int getId();
-			/*
-				returns Id
-			*/
-};
-#endif
+unsigned int VendingMachine::getId() {
+/*
+	returns Id
+*/
+}
