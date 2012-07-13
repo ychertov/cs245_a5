48a0de319ccd25d45e496b0fc9e643ca4c8a5d2f
ychertov <yevgeny.chertov@gmail.com>
2012-07-13 01:47:05 -0400

    fixed stupid mistakes

--- printdatastore.cc
+++ printdatastore.cc
@@ -1,23 +1,23 @@
 #include "printdatastore"
 #include <assert.h>
 
-void setState(char st) {
+void setData(char st) {
 	assert(!state.first);
 	state.first = true;
 	state.second = st;
 }
 
-void setState(char st, int v1) {
+void setData(char st, int v1) {
 	assert(!value1.first);
-	setState(st);
+	setData(st);
 	
 	value1.first = true;
 	value1.second = v1;
 }
 
-void setState(char st, int v1, int v2) {
+void setData(char st, int v1, int v2) {
 	assert(!value2.first);
-	setState(st, v1);
+	setData(st, v1);
 
 	value2.first = true;
 	value2.second = v2;
@@ -40,6 +40,8 @@ friend std::iostream& operator<<( std::ostream& os, const PrintDataStore& pds ) 
 		os << "," << pds.value2.second;
 
 	os << "\t";
+	
+	pds.clear();
 
 	return os;
 }
--- printdatastore.h
+++ printdatastore.h
@@ -1,21 +1,31 @@
 #ifndef __PRINTDATASTORE_H__
 #define __PRINTDATASTORE_H__
 
+/*
+ *
+ *	class that stores the print data
+ *	
+ *	setData: 	stores the data and prepares for printing 
+ *	clear:		clears the data internally and makes it ready for new input,
+ *             		called after it is printed
+ *
+ *
+ */
 class PrintDataStore {
-//use pair instead of pointee
 	private:
 		stl::pair<bool, char> state;
 		stl::pair<bool, int> value1;
 		stl::pair<bool, int> value2;
+		
+		void clear();
 	
 	public:
 		void setState(char state);
 
-		void setValue1(int value);
+		void setState(char state, int v1);
 
-		void setValue2(int value);
+		void setState(char state, int v1, int v2);
 	
-		void clear();
 };
 
 friend std::iostream& operator<<( std::ostream& os, const PrintDataStore& pds );
