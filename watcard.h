cc3a64ecb1cba8425d1d5da86392c0465dcaa06a
mptardib <m_tardibuono@hotmail.com>
2012-07-22 17:18:58 -0400

    documented watcard.h

--- watcard.h
+++ watcard.h
@@ -9,8 +9,27 @@ class WATCard{//Manages the money associated with the card.
 	public:
 		WATCard();//contructor --> initially has a $0 balance
 		void debit( unsigned int amount);
+			/*
+				*Preconditions:
+					-balance [0,amount)
+					(otherwise a call to standard error)
+				*Postconditions:
+					-balance [0, amount)
+			*/
 		void credit( unsigned int amount);
+			/*
+				*Preconditions:
+					-N/A
+				*Postconditions:
+					-N/A
+			*/
 		unsigned int getBalance() const;
+			/*
+				*Preconditions:
+					-N/A
+				*Postconditions:
+					-N/A
+			*/
 };
 
 #endif //WATCARD_H
