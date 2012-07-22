d4cbeeb43b23ea6d11911096e20ae6ebebc7e88a
ychertov <yevgeny.chertov@gmail.com>
2012-07-21 20:12:48 -0400

    removed bkp

--- truckTest.d
+++
@@ -1,2 +0,0 @@
-truckTest.o: truckTest.cc truck.cc truck.h printer.h nameserver.h \
- bottlingplant.h flavours.h vendingmachine.h watcard.h
--- twatcardoffice.d
+++
@@ -1,4 +0,0 @@
-twatcardoffice.o: twatcardoffice.cc PRNG.h soda.h bottlingplant.h \
- printer.h nameserver.h flavours.h student.h watcardoffice.h watcard.h \
- vendingmachine.h truck.h vendingmachinecardeater.h \
- vendingmachineovercharger.h
