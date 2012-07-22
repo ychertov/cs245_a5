e5f1272031af9f01dc7d06efb1ac41a938988df5
ychertov <yevgeny.chertov@gmail.com>
2012-07-22 14:25:34 -0400

    added printing to student

--- watcardoffice.cc
+++ watcardoffice.cc
@@ -6,7 +6,7 @@
 #include <iostream> 
 
 extern PRNG prng;
-static const int CHANCE = 3; //1 in 4 chance (3 = 4 - 1)
+static const int CHANCE = 4; //1 in 4 chance (3 = 4 - 1)
 
 WATCardOffice::WATCardOffice( Printer &prt ) : prt(prt) {
 	prt.print(KIND, START);
@@ -30,7 +30,7 @@ void WATCardOffice::transfer( unsigned int id, unsigned int amount, WATCard &car
 	}	
 	card.credit(amount);
 	prt.print(KIND, END_TRANSFER, id, amount);
-	std::cout << "End Balance: " << card.getBalance();
+	DEBUG(std::cout << "End Balance: " << card.getBalance());
 }
 
 WATCardOffice::~WATCardOffice() {
